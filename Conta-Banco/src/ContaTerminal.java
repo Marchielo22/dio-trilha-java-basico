83ef88f0e81795176cefe7fab263b3c623fb2fde
Marchielo22 <marcelo.junior1@uscsonline.com.br>
2025-06-09 20:18:20 -0300

    Final

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Primeiro caso: saldo = 25, valorSolicitado = 18
+        double saldo = 25;
+        double valorSolicitado = 18;
+
+        if (saldo > valorSolicitado) {
+            saldo = saldo - valorSolicitado;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+
+        System.out.println("Saldo atual: " + saldo);
+
+        // Segundo caso: saldo = 15, valorSolicitado = 22
+        saldo = 15;
+        valorSolicitado = 22;
+
+        if (saldo > valorSolicitado) {
+            saldo = saldo - valorSolicitado;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+
+        System.out.println("Saldo atual: " + saldo);
     }
 }
